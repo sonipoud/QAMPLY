c82282cc61bc730cd26c9169d67aa2c253e10a32
Sonika <sonikapoudyal5@gmail.com>
2021-05-11 19:32:45 -0400

    added function in the js file

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,46 +1,59 @@
 
-var questionsEl = document.querySelector("#question");
-var choicesEl = document.querySelector("#choices");
-var resultsEl = document.querySelector("#results");
-var scoresEl = document.querySelector("#scores");
-var questionIndex = 0;
-var correctCount = 0;
-
 var questions = [
     {
         question: "Which one of the following is not a semantic element?",
         choices: ["<header>", "<div>", "<footer>", "<form>"],
-        answer: "<div>",
+        answer: "<div>"
     },
     {
         question: "Which one of the HTML tag doesn't need closing tag?",
         choices: ["<image>", "<p>", "<body>", "<h1>"],
-        answer: "<image>",
+        answer: "<image>"
     },
     {
         question: "What is CSS known as when refering to a human body part?",
         choices: ["skeleton", "brain", "skin", "heart"],
-        answer: "skin",
+        answer: "skin"
     },
     {
         question: "What is the color property of #FF0000",
         choices: ["yellow", "blue", "green", "red"],
-        answer: "red",
+        answer: "red"
     },
 ];
 
-function fourQuestion() {
+var questionsEl = document.querySelector("#questions");
+var choicesEl = document.querySelector("#choices");
+var resultsEl = document.querySelector("#results");
+var timerEl = document.querySelector("#timer");
+// var scoresEl = document.querySelector("#scores");
+
+var questionIndex = 0;
+var correctCount = 0;
+
+function buildQuiz() {
     questionsEl.textContent = questions[questionIndex].question;
 
-    optionListEl.innerHTML = "";
+    choicesEl.innerHTML = "";
+    resultsEl.innerHTML = "";
 
-    var choices = questions[questionIndex].choices;
-    var choicesLenth = choices.length;
-
-    for (var i = 0; i < choicesLenth; i++) {
-        var questionListItem = document.createElement("li");
-        questionListItem.textContent = choices[i];
-        optionListEl.append(questionListItem);
+    for (var i = 0; i < choices.length; i++) {
+        var questionChoicesItem = document.createElement("li");
+        questionChoicesItem.textContent = questions[questionIndex].choices;[i];
+        choicesEl.append(questionChoicesItem);
     }
-    renderQuestion();
+    function nextQuestion() {
+        questionIndex++;
+        buildQuiz();
+    }
 };
+
+function showResults(event) {
+    if (answer === questions[questionIndex].answer){
+        resultsEl.textContent = "Correct";
+    } else {
+        resultsEl.textContent = "Incorrect";
+    }
+}
+
+// submitButton.addEventListener("click", showResults);
