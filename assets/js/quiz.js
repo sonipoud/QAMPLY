d01b2448e444b2f2431cbc34e3799cb20887a897
Sonika <sonikapoudyal5@gmail.com>
2021-05-12 17:57:28 -0400

    added timer function

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -4,10 +4,17 @@ var questionsEl = document.querySelector("#questions");
 var choicesEl = document.querySelector("#choices");
 var resultsEl = document.querySelector("#results");
 var timerEl = document.querySelector("#timer");
+var buttonEl = document.querySelector("#start");
 // var scoresEl = document.querySelector("#scores");
 
+
 var questionIndex = 0;
-var correctCount = 0;
+var count = 0;
+
+var time = 10;
+var intervalId;
+
+
 
 //created an array of questions
 var questions = [
@@ -33,40 +40,73 @@ var questions = [
     },
 ];
 
+function start(){
+    timerEl.textContent = time;
+    intervalId = setInterval(function(){
+        time--;
+        timerEl.textContent = time;
+
+        if (time === 0){
+            finishQuiz();
+        }
+    }, 1000);
+    
+    buildQuiz();
+};
+
 //added function of bulding the quiz
 function buildQuiz() {
     questionsEl.textContent = questions[questionIndex].question;
 
     choicesEl.innerHTML = "";
     resultsEl.innerHTML = "";
-
+    
     //for loop method
-    for (var i = 0; i < choices.length; i++) {
+    for (var i = 0; i < questions[questionIndex].choices.length; i++) {
         var questionChoicesItem = document.createElement("li");
         questionChoicesItem.textContent = questions[questionIndex].choices[i];
+        questionChoicesItem.addEventListener("click", function(event){
+            showResults(event.target.innerText);
+        });
         choicesEl.append(questionChoicesItem);
     }
     //added function for the atomatic next question after the time interval 
-    function nextQuestion() {
-        questionIndex++;
-    }
+    // function nextQuestion() {
+    //     questionIndex++;
+    // }
+    // if (time === 0) {
+    //     updateTime();
+    //     return;
+    // }
+    //putting time interval between questions
+    // setTimeInterval(nextQuestion, 3000);
 };
 
+function nextQuestion() {
+    questionIndex++;
+    buildQuiz();
+}
 //added function of the results if the customer choses correct answer or wrong answer
-function showResults(event) {
-
+function showResults(answer) {
+console.log(answer, questions[questionIndex].answer);
     if (answer === questions[questionIndex].answer){
         resultsEl.textContent = "Correct";
         console.log(answer);
-        correctCount++;
+        count++;
+        nextQuestion();
     } else {
         resultsEl.textContent = "Incorrect";
+        nextQuestion();
     }
-    //putting time interval between questions
-    setTimeInterval(nextQuestion, 3000);
-}
+};
 
-buildQuiz();
-// submitButton.addEventListener("click", showResults);
+// added function 
+function finishQuiz () {
+    clearInterval(intervalId);
+    var game = document.game;
+    game.innerHTML = " You have scored " + count;
+}
+// buildQuiz();
+buttonEl.addEventListener("click", start);
 
 
